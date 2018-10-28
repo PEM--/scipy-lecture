0570436611d2f62912b5873f3fdd3002118b9569
PEM-- <pemarchandet@gmail.com>
2018-10-28 12:25:55 +0100

    Wallis formula - PI approximation

---
+++ 01_02_03-Wallis-formula/main.py
@@ -0,0 +1,14 @@
+print('Approximation of PI using Wallis formula')
+
+def wallis_step(i):
+  return 4 * i**2 / (4 * i**2 - 1)
+
+def wallis(max_i):
+  res = float(wallis_step(1))
+  for i in range(2, max_i + 1):
+    res = res * float(wallis_step(i))
+  return 2 * res
+
+print(f"10       => {wallis(10)}")
+print(f"100      => {wallis(100)}")
+print(f"1_000    => {wallis(1_000)}")
